836ff4af2a6a4aabcfb47369e7bdf09bd1e666a2
zdflower <zdflower@users.noreply.github.com>
2017-06-28 11:16:36 -0300

    Aparece un bug cuando te equivocás.
    Limpié un poco el código, de todos modos falta.
    Al mostrarse la secuencia y al clickear el jugador los botones cambian el color de la misma forma y suenan los mismos efectos.

--- simon.js
+++ simon.js
@@ -1,3 +1,41 @@
+/* FALTA
+
+**************** 
+hay un bug que aparece cuando te equivocás y después de que te vuelven a mostrar la secuencia empezás a clickear los botones.
+****************
+
+
+- podría haber un botón con un evento toggle que cuando lo clickeás una vez pone on en true
+- revisar todo el código javascript, tal vez algunas de las cosas que están acá sueltas podrían ser parte de la clase simon
+- limpiar el código
+- mejorar el aspecto de la página web
+- en el ejemplo que se muestra en fcc hay un tiempo para que el jugador responda, pasado ese período se da por equivocada la jugada
+- una vez que estén resueltos todos los demás problemas setear la meta en 20 etapas en lugar de 4 o 5 como es ahora.
+*/
+
+/* 
+	Hay varios estados:
+		el de mostrar secuencia
+		el de input
+		el de configuración del modo
+*/
+
+var sim;
+var jugador;
+
+var on = false; //esto para cuando haya un botón de encendido y si on es false se puede cambiar el modo del juego y después clickear en start.
+var estricto = false;
+var restart = false;
+var input = true;
+
+var sonido = document.getElementById('audio');
+var SOUNDmap = {'red': 'simonSound1.mp3', 'blue': 'simonSound2.mp3', 'green': 'simonSound3.mp3', 'yellow': 'simonSound4.mp3'};
+var COLORESmap = {'red': 'rgba(255, 0, 0, 0.5)', 'blue': 'rgba(0,0,255,0.5)', 'green': 'rgba(0,255,0,0.5)', 'yellow': 'rgba(255,255,0,0.2'};
+
+var jugadaOK = true;
+var chequeados = 0;
+
+
 class Simon {
 	constructor(){
 		//private properties
@@ -23,180 +61,13 @@ class Simon {
 	}
 }
 
-//elige al azar una de las opciones y la devuelve
+//elige al azar una de las opciones (un array) y la devuelve
 //opciones es un array de cosas, se devuelve algo del tipo cosa
 //¿o conviene devolver el índice?
 function chooseOne(options){
 	var res = Math.floor(Math.random() * options.length);
 	return options[res];
 }
-
-function checkPlay(seq1, seq2){ //revisar que funcione como supongo
-	//compara dos arrays de strings si para cada elemento y posición en uno y en el otro coinciden
-	var res = seq1.length === seq2.length;//en principio deberían tener la misma longitud
-	var i = 0;
-	while(res  && i < seq1.length){ //empieza si al menos tienen la misma longitud
-		res = seq1[i] === seq2[i];
-		i++;
-	}
-	//cuando sale, por el motivo que sea, devolvés res.
-	return res; 
-}
-
-//export default Simon;
-
-
-function testChooseOne(){
-	var opciones = ['rojo', 'verde', 'azul', 'amarillo'];
-	console.log("Testeando chooseOne");
-	console.log(chooseOne(opciones));
-}
-
-function testSimon(){
-	var sim = new Simon();
-	for (var i = 0; i < 20; i++){
-		console.log("Nueva etapa");
-		sim.addNewStep();
-		console.log(sim.getSequence());
-		console.log(sim.getStage());
-	}
-}
-
-function game(){
-	var mode = 'normal'; //o estricto
-	var player = [];
-	var restart = false;
-	var jugadaOk = true; //cuando empieza el juego el jugador no se equivocó.
-	var sim;
-
-	//pedir input al usuario para saber qué modo quiere
-	//el modo lo vamos a usar cuando el jugador se equivoque.
-
-	//crear el objeto simon
-	sim = new Simon();
-	//mientras restart sea false y la etapa sea menor que 20
-		//agregaretapa
-		//mostrar secuencia
-		//pedir input al usuario 
-		//mientras el usuario no se equivoque y restart sea false
-			//agregaretapa
-			//mostrar secuencia
-			//pedir input
-		//saliste del loop interno, entonces
-		//si el usuario se equivocó
-			//si el modo es estricto, mostrar mensaje de error y reiniciar el juego, podría ponerse restart en true
-		//si restart es true
-			//resetear el juego, eso va a hacer que entre en el loop	
-
-
-	while (sim.getStage() < 5 && !restart ){//5 vueltas para testear
-		sim.addNewStep();
-		console.log("Etapa: " + sim.getStage());
-		console.log(sim.getSequence());
-		mostrarSecuencia(sim);
-		player = sim.getSequence();//prompt("Secuencia: ");
-		jugadaOk = checkPlay(sim.getSequence(), player);
-		//restart = prompt("Restart?: ");
-		while (!jugadaOk && !restart){
-			mostrarMsgError();
-			if (mode = 'strict'){
-				restart = true;
-			} else {
-				console.log("Etapa: " + sim.getStage());
-				console.log(sim.getSequence());
-				mostrarSecuencia(sim);
-				player = sim.getSequence();//prompt("Secuencia: ");
-				jugadaOk = checkPlay(sim.getSequence(), player);
-				//restart = prompt("Restart?: ");		
-			}
-		}
-	}
-	//saliste o porque llegaste a 20 etapas o pusiste restart
-	if (!restart){
-		mostrarMsgGano();
-	}
-}
-
-function mostrarMsg(msg){
-	console.log(msg);
-    $('#msg').css('display', 'block');
-    $('#msg').text(msg);
-    setTimeout(function(){
-        $('#msg').css('display', 'none');
-    }, 3000);
-    
-}
-
-function mostrarMsgGano(){
-   mostrarMsg('¡Ganaste!');
-}
-
-function mostrarMsgError(){
-	mostrarMsg("Te equivocaste. Volvé a intentar.");    
-}
-
-function jugar(){
-	var i = 0;
-	while(i < 5){ //para testear 5 restarts
-		game();
-		i++;
-	}
-
-}
-
-function testCheckPlay(){
-	var a1 = ['rojo', 'azul', 'azul'];
-	var a2 = ['rojo', 'azul', 'azul'];
-	console.log("true: " + checkPlay(a1,a2));
-}
-
-//testChooseOne();
-//testSimon();
-//testCheckPlay();
-//jugar();
-//game();
-//export {chooseOne};
-
-/*
-*/
-/* FALTA
--después de mostrar el mensaje de error durante unos instantes y cuando empieza a mostrar de nuevo la secuencia borrar el mensaje de error.
--después de mostrar el mensaje que ganaste, antes de volver a empezar también debería borrarse el msg
-- antes de volver a empezar que haya unos segundos de espera.
-- que también haya una pausa si te equivocaste antes de que te vuelva a mostrar la secuencia
-- también antes de que muestre la etapa debería pausar unos segundos
-- los colores con los que se muestran los clicks para la secuencia del simon, hacer que sean como los de un click y no los que puse yo.
-- una vez que estén resueltos todos los demás problemas setear la meta en 20 etapas en lugar de 4 o 5 como es ahora.
-
--revisar todo el código javascript, tal vez algunas de las cosas que están acá sueltas podrían ser parte de la clase simon
-
-- ubicar todo este script js en un archivo aparte
-- mejorar el aspecto de la página web
-- limpiar el código
-
-en el ejemplo que se muestra en fcc hay un tiempo para que el jugador responda, pasado ese período se da por equivocada la jugada
-*/
-
-//tenés varios estados:
-//el de mostrar secuencia
-//el de input
-//el de configuración del modo
-
-var sonido = document.getElementById('audio');
-var COLORESmap = {'red': 'pink', 'blue': 'skyblue', 'green': 'lime', 'yellow': 'black'};
-    
-var SOUNDmap = {'red': 'simonSound1.mp3', 'blue': 'simonSound2.mp3', 'green': 'simonSound3.mp3', 'yellow': 'simonSound4.mp3'};
-
-var sim;
-var estricto = false;
-var restart = false;
-var input = true;
-var jugadaOK = true;
-var jugador;
-var chequeados = 0;
-var on = false; //esto para cuando haya un botón de encendido y si on es false se puede cambiar el modo del juego y después clickear en start.
-
-//podría haber un botón con un evento toggle que cuando lo clickeás una vez pone on en true
 
 function start(){
 	//ok, pero cómo interrumpís una vez que empezó un juego? con el botón de on/off
@@ -258,6 +129,11 @@ function clickButton(elem){
     }, 300);
 }
 
+function playSound(snd){
+    sonido.setAttribute('src', SOUNDmap[snd]);
+	sonido.play();
+}
+
 function manejarJugadaErronea(){
 	input = false;
 	mostrarMsgError();
@@ -279,11 +155,24 @@ function changeMode(){
 	}
 }
 
-function playSound(snd){
-    //¿de dónde salió sonido?
-    sonido.setAttribute('src', SOUNDmap[snd]);
-	sonido.play();
+function mostrarMsg(msg){
+	console.log(msg);
+    $('#msg').css('display', 'block');
+    $('#msg').text(msg);
+    setTimeout(function(){
+        $('#msg').css('display', 'none');
+    }, 3000);
+    
 }
+
+function mostrarMsgGano(){
+   mostrarMsg('¡Ganaste!');
+}
+
+function mostrarMsgError(){
+	mostrarMsg("Te equivocaste. Volvé a intentar.");    
+}
+
     
 $(document).ready(function(){
 
@@ -293,9 +182,8 @@ $(document).ready(function(){
 	$('.colores').on('click', function(){
 		if(on){
 		    var color = $(this).attr('id');
-
-		    /* efecto de sonido al clickear */
-            playSound(color);
+            
+            clickButton(color);
 
 		    console.log(color);
 			console.log("input: ", input);
